518bc5c9e47de663e999f0973826f4fa6793e6e1
pulungrwo <pulungrwo@gmail.com>
2025-09-25 01:16:44 +0700

    Update budidaya.js

--- budidaya.js
+++ budidaya.js
@@ -1,35 +1,84 @@
 // Data budidaya
 const budidayaData = [
+{
+  tanaman: "Singkong",
+  tanggal: "2024-07-05",
+  luas: "1.7 Ha",
+  umur: "6 Bulan",
+  hasil: { jumlah: 37, satuan: "Ton" },
+  omzet: 31500000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2023-12-01",
+  luas: "4 Rante (1600 m²)",
+  umur: "5 Bulan",
+  hasil: { jumlah: 1, satuan: "Ton" },
+  omzet: 1200000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2023-06-05",
+  luas: "4 Rante (1600 m²)",
+  umur: "10 Bulan",
+  hasil: { jumlah: 4.5, satuan: "Ton" },
+  omzet: 3500000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2022-10-15",
+  luas: "8 Rante (3200 m²)",
+  umur: "8 Bulan",
+  hasil: { jumlah: 6, satuan: "Ton" },
+  omzet: 5000000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2022-08-01",
+  luas: "4 Rante (1600 m²)",
+  umur: "11 Bulan",
+  hasil: { jumlah: 4, satuan: "Ton" },
+  omzet: 3000000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2022-08-05",
+  luas: "5 Rante (2000 m²)",
+  umur: "5 Bulan",
+  hasil: { jumlah: 3, satuan: "Ton" },
+  omzet: 2500000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2022-02-15",
+  luas: "8 Rante (3200 m²)",
+  umur: "6 Bulan",
+  hasil: { jumlah: 6, satuan: "Ton" },
+  omzet: 6000000,
+  foto: ""
+},
+{
+  tanaman: "Singkong",
+  tanggal: "2021-09-01",
+  luas: "4 Rante (1600 m²)",
+  umur: "10 Bulan",
+  hasil: { jumlah: 4, satuan: "Ton" },
+  omzet: 3000000,
+  foto: ""
+},
   {
-    tanaman: "Jagung",
-    tanggal: "2025-03-12",
-    luas: "0.5 Ha",
-    hasil: { jumlah: 3, satuan: "Ton" },
-    umur: "100 Hari",
-    foto: "jagung.jpg"
-  },
-  {
-    tanaman: "Timun",
-    tanggal: "2025-05-10",
-    luas: "0.25 Ha",
-    hasil: { jumlah: 1200, satuan: "Kg" },
-    umur: "60 Hari",
-    foto: "timun.jpg"
-  },
-  {
-    tanaman: "Cabai",
-    tanggal: "2025-08-20",
-    luas: "0.3 Ha",
-    hasil: { jumlah: 850, satuan: "Kg" },
-    umur: "90 Hari",
-    foto: "cabai.jpg"
-  },
-  {
-    tanaman: "Singkong",
-    tanggal: "2025-09-15",
-    luas: "1 Ha",
-    hasil: { jumlah: 52, satuan: "Ton" },
-    umur: "9 Bulan",
-    foto: "singkong.jpg"
-  }
+  tanaman: "Singkong",
+  tanggal: "2021-08-10",
+  luas: "5 Rante (2000 m²)",
+  umur: "5 Bulan",
+  hasil: { jumlah: 3, satuan: "Ton" },
+  omzet: 2500000,
+  foto: ""
+},
 ];
