17d046fdff1e95a29803b951309cafe5f6be1f10
pulungrwo <pulungrwo@gmail.com>
2025-10-10 21:03:20 +0700

    Create peserta.js

---
+++ kreator/peserta.js
@@ -0,0 +1,16 @@
+const pesertaData = [
+  {
+    nama: "@tanjung_01",
+    telepon: "081234567890",
+    merekHP: "Oppo A78",
+    caption: "Video tentang kehidupan dusun yang inspiratif",
+    linkVideo: "https://vt.tiktok.com/ZS123abc/"
+  },
+  {
+    nama: "@bulan_02",
+    telepon: "089876543210",
+    merekHP: "Samsung A24",
+    caption: "Konten edukasi seputar pertanian lokal",
+    linkVideo: "https://www.tiktok.com/@bulan_02/video/7351234567"
+  }
+];
