305ccff9f904f3486cc78f1dbe301b37ca276a48
pulungrwo <pulungrwo@gmail.com>
2026-01-14 00:41:36 +0700

    Update rismafarm.js

--- rismafarm.js
+++ rismafarm.js
@@ -1,1 +1,26 @@
-.
+const RISMA_FARM = {
+  nama: "RISMA FARM",
+
+  musim: {
+    3: {
+      label: "Musim Tanam Ke-3",
+      modal: { risma: 500000 },
+      biaya: [],
+      panen: []
+    },
+
+    2: {
+      label: "Musim Tanam Ke-2",
+      modal: {},
+      biaya: [],
+      panen: []
+    },
+
+    1: {
+      label: "Musim Tanam Ke-1",
+      modal: {},
+      biaya: [],
+      panen: []
+    }
+  }
+};
