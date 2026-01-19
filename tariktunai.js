38c6feaa401665d65684a30f9a3a3d3c594e9331
pulungrwo <pulungrwo@gmail.com>
2026-01-20 01:02:39 +0700

    Delete tariktunai.js

--- tariktunai.js
+++
@@ -1,3 +0,0 @@
-// tariktunai.js // DATA TARIK TUNAI ANGGOTA BANK RISMA // DATA TERBARU SELALU DI ATAS
-
-const tarikTunai = [ // { //   tanggal: "2025-04-15", //   nama: "Pulung", //   jumlah: 200000 // }, ];
