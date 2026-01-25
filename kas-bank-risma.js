1db6fee26052689255fb6bf5d93cec782c121d80
pulungrwo <pulungrwo@gmail.com>
2026-01-25 21:35:16 +0700

    Update kas-bank-risma.js

--- kas-bank-risma.js
+++ kas-bank-risma.js
@@ -66,3 +66,27 @@ const KAS_BANK_RISMA = {
 
   ]
 };
+
+
+
+(function buildBankRisma() {
+  let saldo = 0;
+  const anggota = {};
+
+  Object.values(KAS_BANK_RISMA).forEach(list => {
+    list.forEach(tx => {
+      const faktor = tx.tipe === "masuk" ? 1 : -1;
+
+      Object.entries(tx.detail || {}).forEach(([nama, nilai]) => {
+        const v = Number(nilai) * faktor;
+        saldo += v;
+        anggota[nama] = (anggota[nama] || 0) + v;
+      });
+    });
+  });
+
+  window.BANK_RISMA = {
+    saldo,
+    anggota
+  };
+})();
