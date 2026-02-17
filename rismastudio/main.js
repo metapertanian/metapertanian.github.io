4bbdb73f3e1d329f4003650187343a64edd76c79
pulungrwo <pulungrwo@gmail.com>
2026-02-17 14:43:15 +0700

    Update main.js

--- rismastudio/main.js
+++ rismastudio/main.js
@@ -1,25 +1,24 @@
-window.onload = function () {
+let persen = 0;
 
-  let persen = 0;
+const loadingText = document.getElementById("loadingPercent");
+const loadingScreen = document.getElementById("loadingScreen");
+const mainContent = document.getElementById("mainContent");
 
-  const loadingText = document.getElementById("loadingPercent");
-  const loadingPopup = document.getElementById("loadingPopup");
+const interval = setInterval(function(){
 
-  if (!loadingText || !loadingPopup) {
-    console.log("Element loading tidak ditemukan");
-    return;
+  persen += 5;
+  loadingText.innerText = persen;
+
+  if(persen >= 100){
+    clearInterval(interval);
+
+    loadingScreen.style.display = "none";
+    mainContent.style.display = "block";
+
+    // Load Navbar setelah loading selesai
+    fetch("navbar.html")
+      .then(r=>r.text())
+      .then(d=>document.getElementById("navbar").innerHTML=d);
   }
 
-  const interval = setInterval(function () {
-
-    persen += 5;
-    loadingText.innerText = persen;
-
-    if (persen >= 100) {
-      clearInterval(interval);
-      loadingPopup.style.display = "none";
-    }
-
-  }, 150);
-
-};
+}, 100);
