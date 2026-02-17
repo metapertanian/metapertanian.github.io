50c42c2aac5543005c9680a7e32d8cad7636dbfa
pulungrwo <pulungrwo@gmail.com>
2026-02-17 13:59:27 +0700

    Update main.js

--- rismastudio/main.js
+++ rismastudio/main.js
@@ -1,20 +1,26 @@
-import "./firebase.js";
-import "./app.js";
+// LOADING MURNI TANPA IMPORT APAPUN
 
-// Loading animation 3 detik
-let persen = 0;
+window.addEventListener("load", function () {
 
-window.addEventListener("DOMContentLoaded", () => {
+  let persen = 0;
   const loadingText = document.getElementById("loadingPercent");
   const loadingPopup = document.getElementById("loadingPopup");
 
   const interval = setInterval(() => {
+
     persen += 2;
-    if (loadingText) loadingText.innerText = persen;
+
+    if (loadingText) {
+      loadingText.innerText = persen;
+    }
 
     if (persen >= 100) {
       clearInterval(interval);
-      if (loadingPopup) loadingPopup.style.display = "none";
+      if (loadingPopup) {
+        loadingPopup.style.display = "none";
+      }
     }
+
   }, 60);
+
 });
