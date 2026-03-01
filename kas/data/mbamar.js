9cc9c59868683ec643657acfd1f3ec7d9b214a89
pulungrwo <pulungrwo@gmail.com>
2026-03-01 10:55:44 +0700

    Update mbamar.js

--- kas/data/mbamar.js
+++ kas/data/mbamar.js
@@ -2,7 +2,90 @@
 // Penyimpanan data arus kas per tahun
 
 window.kasData = {  
+  "2026": {  
+    periode: "Januari 2026 – Desember 2026",  
+    bendahara: "Pulung Riswanto",  
+    transaksi: [  
 
+{
+date: "2026-02-05",
+type: "income",
+amount: 1600000
+},
+{
+date: "2026-02-05",
+type: "expense",
+amount: 500000,
+description: "Ris"
+},
+{
+date: "2026-02-05",
+type: "expense",
+amount: 60000,
+description: "Pulung"
+},
+{
+date: "2026-02-05",
+type: "expense",
+amount: 700000,
+description: "Setoran"
+},
+{
+date: "2026-02-05",
+type: "expense",
+amount: 200000,
+description: "Bapak"
+},
+{
+date: "2026-02-05",
+type: "expense",
+amount: 140000,
+description: "Sasa"
+},
+{
+date: "2026-01-05",
+type: "income",
+amount: 1000000,
+note: "di pinjam wahyu 300 dibalikin 250, dikasih wahyu 100. sisa 150"
+},
+{
+date: "2026-01-05",
+type: "expense",
+amount: 300000,
+description: "Dipinjam Wahyu"
+},
+{
+date: "2026-01-15",
+type: "income",
+amount: 250000,
+description: "Dibalikin Wahyu"
+},
+{
+date: "2026-01-21",
+type: "expense",
+amount: 700000,
+description: "Setoran"
+},
+{
+date: "2026-01-25",
+type: "expense",
+amount: 100000,
+description: "Wahyu"
+},
+{
+date: "2026-02-28",
+type: "income",
+amount: 500000,
+description: "Ris Bayar"
+},
+{
+date: "2026-03-01",
+type: "expense",
+amount: 500000,
+description: "Wahyu"
+}
+]
+},
 
 };  
 
