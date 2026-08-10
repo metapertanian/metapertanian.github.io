1f7f18e6cccb5a58ce61c6fde35250df86af01ce
pulungrwo <pulungrwo@gmail.com>
2026-08-10 17:10:16 +0700

    Update masjid.js

--- kas/data/masjid.js
+++ kas/data/masjid.js
@@ -9,6 +9,18 @@ window.kasData = {
     bendahara: "Pulung Riswanto",  
     transaksi: [  
 {
+date: "2026-08-07",
+type: "income",
+amount: 26000,
+description: "Infak Jumat"
+},
+{
+date: "2026-08-07",
+type: "expense",
+amount: 25000,
+description: "Marbot"
+},
+{
 date: "2026-07-31",
 type: "income",
 amount: 30000,
